decd11fb2f630e6c1fc9918a7df61c171f165e56
Ned Ruggeri <ruggeri@self-loop.com>
2012-09-12 08:00:30 -0700

    make sure to respond with json to requests

--- app/controllers/diary_entries_controller.rb
+++ app/controllers/diary_entries_controller.rb
@@ -1,10 +1,13 @@
 class DiaryEntriesController < ApplicationController
   before_filter :authenticate_user!
+  respond_to :html, :json
 
   def index
     @user = current_user
 
     @entries = @user.diary_entries
+
+    respond_with(@entries)
   end
 
   def new
