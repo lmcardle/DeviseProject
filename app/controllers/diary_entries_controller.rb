60decd53547f9c4720a2cabe5e8bd012d92bc37c
Ned Ruggeri <ruggeri@self-loop.com>
2012-09-12 07:15:35 -0700

    add ability to create new diary entries

--- app/controllers/diary_entries_controller.rb
+++ app/controllers/diary_entries_controller.rb
@@ -1,5 +1,24 @@
 class DiaryEntriesController < ApplicationController
+  before_filter :authenticate_user!
+
   def index
-    @entries = DiaryEntry.all
+    @user = current_user
+
+    @entries = @user.diary_entries
+  end
+
+  def new
+    @user = current_user
+    @diary_entry = @user.diary_entries.build
+  end
+
+  def create
+    @diary_entry = DiaryEntry.new(params[:diary_entry])
+    @diary_entry.save
+
+    # should check @diary_entry.user == current_user
+    # else anyone can post as another user...
+
+    redirect_to diary_entries_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 DeviseProject::Application.routes.draw do
   devise_for :users
 
-  resources :diary_entries, :only => [:index]
+  resources :diary_entries, :only => [:create, :index, :new]
 
   root :to => 'static_pages#home'
 end
