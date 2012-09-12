76b636f0e35f759a11833a1a179534f764e99f93
Ned Ruggeri <ruggeri@self-loop.com>
2012-09-12 16:20:47 -0700

    set user_id of a new diary entry only right before object saved
    avoids problems of changing the hidden field, simplifies uploaded object

--- app/controllers/diary_entries_controller.rb
+++ app/controllers/diary_entries_controller.rb
@@ -4,23 +4,19 @@ class DiaryEntriesController < ApplicationController
 
   def index
     @user = current_user
-
     @entries = @user.diary_entries
 
     respond_with(@entries)
   end
 
   def new
-    @user = current_user
-    @diary_entry = @user.diary_entries.build
+    @diary_entry = DiaryEntry.new
   end
 
   def create
     @diary_entry = DiaryEntry.new(params[:diary_entry])
+    @diary_entry.user_id = current_user.id
     @diary_entry.save
-
-    # should check @diary_entry.user == current_user
-    # else anyone can post as another user...
 
     redirect_to diary_entries_path
   end
